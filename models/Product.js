baa4d7c16f75a72b457bad8c5c97897caa97e357
Falk König <fk@falkkoenig.de>
2024-03-07 16:24:47 +0100

    add top product

--- models/Product.js
+++ models/Product.js
@@ -6,10 +6,11 @@ const productSchema = new Schema(
   {
     title: { type: String, required: true },
     category: { type: String, required: false },
-    description: { type: Number, required: false },
+    description: { type: String, required: false },
     timestamp: { type: Date, required: true },
     stock: { type: Number, required: false },
     price: { type: Number, required: false },
+    top: { type: Boolean, required: false },
   },
   { versionKey: false }
 );
