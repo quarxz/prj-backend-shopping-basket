f442e3974aac1e0959b93cf029f7beb356828616
Falk König <fk@falkkoenig.de>
2024-03-14 19:55:31 +0100

    add/delete/update shopping basket

--- controller/userController.js
+++ controller/userController.js
@@ -117,7 +117,7 @@ const userByProduct = async (req, res) => {
         for (let i = 0; i < user.products.length; i++) {
           let el = user.products[i].productId;
           if (el.toString() === isProductExists.toString()) {
-            return res.status(400).json({ message: "Book already bought!" });
+            return res.status(400).json({ message: "Produkt already in basket!" });
           }
         }
       }
@@ -165,20 +165,10 @@ const userDeleteProduct = async (req, res) => {
     const user = await User.findOne({ _id: userId });
 
     const { productId, quantity } = req.body;
+    console.log(quantity);
 
     if (user && productId) {
       const { _id: isProductExists } = (await Product.findOne({ _id: productId })) || { _id: null };
-
-      if (user.products.length) {
-        for (let i = 0; i < user.products.length; i++) {
-          let el = user.products[i];
-          if (el.productId.toString() === isProductExists.toString()) {
-            if (quantity > el.quantity) {
-              return res.status(400).json({ message: "Max to delete: " + el.quantity });
-            }
-          }
-        }
-      }
 
       const product = await Product.findOne({ _id: productId });
 
@@ -187,13 +177,48 @@ const userDeleteProduct = async (req, res) => {
         { $inc: { stock: +quantity } }
       );
 
-      const updateUser = await User.findByIdAndUpdate(
-        userId,
-        { $pull: { products: { productId: productId } } },
-        { returnDocument: "after" }
-      );
+      // const updateUserProduct02 = user.products.map((product) =>
+      //   product.productId === productId ? { ...product, quantity: product.quantity - 2 } : product
+      // );
+      // console.log(updateUserProduct02);
 
-      return res.status(200).json(updateUser);
+      // const updateUserProduct02 = user.products.map((product) =>
+      //   product.productId.toString() === productId.toString() ? product.quantity : ""
+      // );
+      // console.log("x", updateUserProduct02.filter(Number));
+
+      // const { products } = await User.findByIdAndUpdate({ _id: userId });
+      // console.log(products);
+      // const updateUser = await User.find({ userId: userId });
+
+      user.products.map(async (product) => {
+        if (product.productId.toString() === productId.toString()) {
+          if (product.quantity >= 1) {
+            // Update if basket quantity > want to delete quantity
+            const updateUserProduct = await User.findOneAndUpdate(
+              { _id: userId },
+              { $inc: { "products.$[filter].quantity": -quantity } },
+              { arrayFilters: [{ "filter.productId": productId }] },
+              { returnDocument: "after" }
+            );
+            // Update if basket quantity = 0 after want to delete
+            const { products } = await User.findOne({ _id: userId });
+            products.map(async (product) => {
+              if (product.productId.toString() === productId.toString()) {
+                if (product.quantity < 1) {
+                  const updateUser = await User.findByIdAndUpdate(
+                    userId,
+                    { $pull: { products: { productId: productId } } },
+                    { returnDocument: "after" },
+                    { returnNewDocument: true }
+                  );
+                  return res.status(200).json(updateUser);
+                }
+              }
+            });
+          }
+        }
+      });
     } else {
       return res.status(400).json({
         message: "Product NOT deleted. Product Id and/or User id is missing!",
